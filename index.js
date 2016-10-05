7f9d121844beb1ec0c488c7ed5c0542fe885e815
Thiago Bocchile <tykoth@gmail.com>
2016-10-05 14:09:15 -0300

    Proxy server test na porta 3303

--- index.js
+++ index.js
@@ -37,3 +37,27 @@ http.listen(80, function(){
 	//'http://localhost.com:3000'
   //]);  
 });
+
+
+// Hello darkness my old friend.
+var http2 = require('http');
+
+http2.createServer(function(request, response) {
+  var proxy = http2.createClient(80, request.headers['host'])
+  var proxy_request = proxy.request(request.method, request.url, request.headers);
+  proxy_request.addListener('response', function (proxy_response) {
+    proxy_response.addListener('data', function(chunk) {
+      response.write(chunk, 'binary');
+    });
+    proxy_response.addListener('end', function() {
+      response.end();
+    });
+    response.writeHead(proxy_response.statusCode, proxy_response.headers);
+  });
+  request.addListener('data', function(chunk) {
+    proxy_request.write(chunk, 'binary');
+  });
+  request.addListener('end', function() {
+    proxy_request.end();
+  });
+}).listen(3303);
